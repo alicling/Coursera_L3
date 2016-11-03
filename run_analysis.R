3f8b4b558c11489e3e4f52a1a6f4ce86916bb898
alicling <xli30@tulane.edu>
2016-11-02 22:42:21 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,79 @@
-#change work dir
-setwd("C:/Users/xiang/Documents/UCI HAR Dataset")
-#read files
-training_set<-read.table("./train/X_train.txt")
-test_set<-read.table("./test/X_test.txt")
-dataset<-merge(training_set,test_set)
-#load package
-library(dplyr)
-library(data.table)
-library(tidyr)
-test_set<-tbl_df(test_set)
-training_set<-tbl_df(training_set)
-dataset<-rbind(test_set,training_set)
-#adjust colume names
-col_names<-tbl_df(read.csv("./features.txt",header = FALSE))
-#filter columes name
-col<-filter(col_names,!nchar(as.character(col_names$V1))<3)
-col<-unique(col)
-col<-col[-c(556,558),]
-colnames(dataset)<-t(col)
-#select columes
-dataset_select1<-select(dataset,contains("mean()"))
-dataset_select2<-select(dataset,contains("std()"))
-dataset_select<-cbind(dataset_select1,dataset_select2)
-#split the names
-splitnames<-strsplit(names(dataset_select),"\\ ")
-secondelement<-function(x){x[2]}
-newnames<-sapply(splitnames,secondelement)
-colnames(dataset_select)<-newnames
-#change names into tidy names
-names(dataset_select)<-gsub("mean()", "MEAN", names(dataset_select))
-names(dataset_select)<-gsub("^t", "time", names(dataset_select))
-names(dataset_select)<-gsub("^f", "frequency", names(dataset_select))
-names(dataset_select)<-gsub("Acc", "Accelerometer", names(dataset_select))
-names(dataset_select)<-gsub("Gyro", "Gyroscope", names(dataset_select))
-names(dataset_select)<-gsub("Mag", "Magnitude", names(dataset_select))
-names(dataset_select)<-gsub("BodyBody", "Body", names(dataset_select))
-#write a new table and name as "tidy data"
-write.table(dataset_select, "TidyData.txt", row.name=FALSE)
+# Run the analysis -> creates the tiny data set and saves it under "tiny.txt"
+
+# Reads the base sets (files with begining by X) in an optimal way
+# * filePath: path of the file
+# * filteredFeatures: ids of the features to be extracted
+# * features: all features names
+readBaseSet <- function(filePath, filteredFeatures, features) {
+        cols_widths <- rep(-16, length(features))
+        cols_widths[filteredFeatures] <- 16
+        rawSet <- read.fwf(
+                file=filePath,
+                widths=cols_widths,
+                col.names=features[filteredFeatures])
+}
+
+# Reads an additional file (other than the base sets). Used for subjects and labels.
+# * dataDirectory: directory of data
+# * filePath: relative path of the file. For instance if its value is "subject" it
+#   will read "UCI HAR Dataset/test/subject_test.txt" and
+# "UCI HAR Dataset/train/subject_train.txt", and merge them
+readAdditionalFile <- function(dataDirectory, filePath) {
+        filePathTest <- paste(dataDirectory, "/test/", filePath, "_test.txt", sep="")
+        filePathTrain <- paste(dataDirectory, "/train/", filePath, "_train.txt", sep="")
+        data <- c(read.table(filePathTest)[,"V1"], read.table(filePathTrain)[,"V1"])
+        data
+}
+
+# Correct a feature name - makes it nicer for dataframe columns (removes parentheses)
+# because otherwise they are transformed to dots.
+# * featureName: name of the feature
+correctFeatureName <- function(featureName) {
+        featureName <- gsub("\\(", "", featureName)
+        featureName <- gsub("\\)", "", featureName)
+        featureName
+}
+
+# Read sets and returns a complete sets
+# * dataDirectory: directory of data
+readSets <- function(dataDirectory) {
+        # Adding main data files (X_train and X_test)
+        featuresFilePath <- paste(dataDirectory, "/features.txt", sep="")
+        features <- read.table(featuresFilePath)[,"V2"]
+        filteredFeatures <- sort(union(grep("mean\\(\\)", features), grep("std\\(\\)", features)))
+        features <- correctFeatureName(features)
+        set <- readBaseSet(paste(dataDirectory, "/test/X_test.txt", sep=""), filteredFeatures, features)
+        set <- rbind(set, readBaseSet(paste(dataDirectory, "/train/X_train.txt", sep=""), filteredFeatures, features))
+        
+        # Adding subjects
+        set$subject <- readAdditionalFile("UCI HAR Dataset", "subject")
+        
+        # Adding activities
+        activitiesFilePath <- paste(dataDirectory, "/activity_labels.txt", sep="")
+        activities <- read.table(activitiesFilePath)[,"V2"]
+        set$activity <- activities[readAdditionalFile("UCI HAR Dataset", "y")]
+        
+        set
+}
+
+# From sets, creates the tidy dataset (a summary)
+# * dataDirectory: directory of data
+createSummaryDataset <- function(dataDirectory) {
+        sets <- readSets(dataDirectory)
+        sets_x <- sets[,seq(1, length(names(sets)) - 2)]
+        summary_by <- by(sets_x,paste(sets$subject, sets$activity, sep="_"), FUN=colMeans)
+        summary <- do.call(rbind, summary_by)
+        summary
+}
+
+dataDirectory <- "UCI HAR Dataset"
+if (!file.exists(dataDirectory)) {
+        url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+        tmp_file <- "./temp.zip"
+        download.file(url,tmp_file, method="curl")
+        unzip(tmp_file, exdir="./")
+        unlink(tmp_file)
+}
+
+summary <- createSummaryDataset(dataDirectory)
+write.table(summary, "tidy.txt")
